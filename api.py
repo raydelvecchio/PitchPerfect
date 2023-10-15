ba85fb81bedcef18e316dc5d86db42bd22173416
Raymond Del Vecchio <raydelv@MackTruck.local>
2023-10-15 00:38:17 -0700

    cross origin policy

--- api.py
+++ api.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from classes import Pulze, DeepGram, Labs11
 import uvicorn
 
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1:80001",
+    "http://127.0.0.1:8001/upload",
+    "http://127.0.0.1:8001/parse",
+    "http://127.0.0.1:8001/generate"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 pulze = Pulze()
 deep = DeepGram()
 TARGET_FILENAME = "TARGET_AUDIO.mp3"
